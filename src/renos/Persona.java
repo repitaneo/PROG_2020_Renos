b28e9f594a658954d4e3224dbf4997cefc08a205
repitaneo <fernandez.marcos@gmail.com>
2020-01-06 14:42:32 +0100

    Reno 1: funcionado según enunciado

---
+++ src/renos/Persona.java
@@ -0,0 +1,66 @@
+package renos;
+
+public class Persona {
+
+	
+	private String nombre;
+	private int edad;
+	private String DNI;
+	private char sexo;
+	private int peso;
+	private int altura;
+	
+	
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public String getDNI() {
+		return DNI;
+	}
+	public void setDNI(String dNI) {
+		DNI = dNI;
+	}
+	public char getSexo() {
+		return sexo;
+	}
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+	public int getPeso() {
+		return peso;
+	}
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
+	public int getAltura() {
+		return altura;
+	}
+	public void setAltura(int altura) {
+		this.altura = altura;
+	}
+	
+	
+	
+	@Override
+	public String toString() {
+		return "Persona [nombre=" + nombre + ", edad=" + edad + ", DNI=" + DNI + ", sexo=" + sexo + ", peso=" + peso
+				+ ", altura=" + altura + "]";
+	}
+	
+	
+	
+	
+	
+	
+}
---
+++ src/renos/Start.java
@@ -0,0 +1,23 @@
+package renos;
+
+public class Start {
+
+	public static void main(String[] args) {
+		
+		
+		Persona marcos = new Persona();
+		
+		marcos.setNombre("Marcos Fernandez");
+		marcos.setPeso(70);
+		marcos.setSexo('H');
+		marcos.setAltura(172);
+		marcos.setDNI("98555333J");
+		marcos.setEdad(31);
+		
+		
+		System.out.println(marcos);
+		
+
+	}
+
+}
